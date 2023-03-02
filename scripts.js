5c12ce169ad14c84eac180d75c07b5d6e2130fc7
Arunachalam Vijayakumar <avijayakumar@presidio.com>
2023-03-02 11:54:06 +0530

    Added Dynamic fetched data to table_body

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
 fetch("https://openlibrary.org/search/authors.json?q=j%20k%20rowling")
-	.then((response) => response.json())
-	.then((data) => console.log(data));
+	.then((response) => {
+		return response.json();
+	}) //converted to object
+	.then((data) => {
+		let tableData = "";
+		let docs = [data.docs];
+		console.log(docs);
+		docs[0].map((doc) => {
+			tableData += `<tr>
+            <td>${doc.key ? doc.key : "-"}</td>
+            <td>${doc.name ? doc.name : "-"}</td>
+            <td>${doc.name ? doc.name : "-"}</td>
+            <td>${doc.name ? doc.name : "-"}</td>
+            <td>${doc.top_subjects ? doc.top_subjects : "-"}</td>
+            <td>${doc.top_work ? doc.top_work : "-"}</td>
+          </tr>`;
+		});
+		document.getElementById("table-body").innerHTML = tableData;
+	});
