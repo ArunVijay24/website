b3ae9d579ca8626dd923ed9f110bcef66a0b6d37
Arunachalam Vijayakumar <avijayakumar@presidio.com>
2023-03-02 22:27:35 +0530

    Added table responsive

--- scripts.js
+++ scripts.js
@@ -1,18 +1,23 @@
 // Fetching API data
-fetch("https://openlibrary.org/search/authors.json?q=j%20k%20rowling")
+fetch("https://openlibrary.org/search/authors.json?q=a")
 	.then((response) => {
 		return response.json();
 	}) //converted to object
 	.then((data) => {
 		let tableData = "";
 		let docs = [data.docs];
-		console.log(docs);
 		docs[0].map((doc) => {
+			const displayname = doc.name;
+			const authorid = doc.key;
 			tableData += `<tr>
-            <td class="text-center">${doc.key ? doc.key : "-"}</td>
-            <td class="text-center">${doc.name ? doc.name : "-"}</td>
-            <td class="text-center">${doc.name ? doc.name : "-"}</td>
-            <td class="text-center">${doc.name ? doc.name : "-"}</td>
+            <td class="text-center">${authorid ? authorid : "-"}</td>
+            <td class="text-center">${
+				displayname ? displayname.split(" ").slice(0, -1).join(" ") : "-"
+			}</td>
+            <td class="text-center">${
+				displayname ? displayname.split(" ").slice(-1).join(" ") : "-"
+			}</td>
+            <td class="text-center">${displayname ? displayname : "-"}</td>
             <td class="text-center">${doc.top_subjects ? doc.top_subjects : "-"}</td>
             <td class="text-center">${doc.top_work ? doc.top_work : "-"}</td>
           </tr>`;
@@ -29,7 +34,7 @@ function filter() {
 	table = document.getElementById("myTable");
 	tr = table.getElementsByTagName("tr");
 	for (i = 0; i < tr.length; i++) {
-		td = tr[i].getElementsByTagName("td")[0];
+		td = tr[i].getElementsByTagName("td")[3];
 		if (td) {
 			txtValue = td.textContent || td.innerText;
 			if (txtValue.toUpperCase().indexOf(filter) > -1) {
